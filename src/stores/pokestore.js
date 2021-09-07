1382f0acd20fa3d9022f8fcd9475c7ef288d3419
Greg Rickaby <gregrickaby@gmail.com>
2021-09-07 13:08:24 -0500

    refactor to use named functions

--- src/stores/pokestore.js
+++ src/stores/pokestore.js
@@ -4,28 +4,49 @@ export const pokemon = writable([]);
 const pokemonDetails = {};
 let loaded = false;
 
-export const fetchPokemon = async () => {
-	if (loaded) return;
-	const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
-	const res = await fetch(url);
-	const data = await res.json();
-	const loadedPokemon = data.results.map((data, index) => ({
-		name: data.name,
-		id: index + 1,
-		image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-			index + 1
-		}.png`
-	}));
-	pokemon.set(loadedPokemon);
-	loaded = true;
-};
-
-export const getPokemonById = async (id) => {
-	if (pokemonDetails[id]) return pokemonDetails[id];
+/**
+ * Fetch the pokemon list from the PokeAPI.
+ *
+ * @see https://svelte.dev/docs#svelte_store
+ * @see https://pokeapi.co/docs/v2
+ */
+export async function fetchPokemon() {
+	if (loaded) {
+		return;
+	}
 
 	try {
-		const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-		const res = await fetch(url);
+		const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=150`);
+		const data = await res.json();
+		const loadedPokemon = data.results.map((data, index) => ({
+			name: data.name,
+			id: index + 1,
+			image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+				index + 1
+			}.png`
+		}));
+		pokemon.set(loadedPokemon);
+		loaded = true;
+	} catch (err) {
+		console.error(err);
+		return null;
+	}
+}
+
+/**
+ * Fetch indivdual pokemon details from the PokeAPI.
+ *
+ * @see https://pokeapi.co/docs/v2
+ * @param {number} id The ID of a pokémon.
+ * @returns {object}  The details of a pokémon.
+ */
+export async function getPokemonById(id) {
+	if (pokemonDetails[id]) {
+		return pokemonDetails[id];
+	}
+
+	try {
+		const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
 		const data = await res.json();
 		pokemonDetails[id] = data;
 		return data;
@@ -33,4 +54,4 @@ export const getPokemonById = async (id) => {
 		console.error(err);
 		return null;
 	}
-};
+}
