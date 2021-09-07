5b1a95d2dc7b53ea9a8950e606d4d1dd912d220c
Greg Rickaby <gregrickaby@gmail.com>
2021-09-07 13:07:46 -0500

    a preconnect and meta desc.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,9 +2,15 @@
 	import Nav from '../components/Nav.svelte';
 </script>
 
+<svelte:head>
+	<link rel="preconnect" href="https://pokeapi.co" />
+	<link rel="preconnect" href="https://raw.githubusercontent.com" />
+	<meta name="description" content="A Pokédex powered by SvelteKit and the PokeAPI." />
+</svelte:head>
+
 <div class="max-w-6xl mx-auto p-8">
 	<Nav />
-	<slot />
+	<slot>No children provided.</slot>
 </div>
 
 <style>
