5636d639297aa13e77b4b1367b69a70aaed514c4
Greg Rickaby <gregrickaby@gmail.com>
2021-09-07 10:06:08 -0500

    use static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,16 @@
 /** @type {import('@sveltejs/kit').Config} */
+
+import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		// https://github.com/sveltejs/kit/tree/master/packages/adapter-static
+		adapter: adapter({
+			assets: 'build',
+			pages: 'build',
+			fallback: null
+		})
 	}
 };
 
